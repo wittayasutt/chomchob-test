7d29ffa20b66cac786fce49d0c6d0c852df06a17
wittayasutt <wittayasutt@gmail.com>
2018-06-06 02:06:32 +0700

    delete setting

--- src/containers/navbar/index.js
+++ src/containers/navbar/index.js
@@ -9,27 +9,8 @@ const NavBar = () => (
 	<Wrapper
 		className="navbar"
 		role="navigation"
-		aria-label="dropdown navigation"
-	>
-		<div className="navbar-menu container">
-			<div className="navbar-end">
-				<div className="navbar-item has-dropdown">
-					<a className="navbar-link">
-						<span className="icon has-text-info">
-							<i className="fas fa-cog" />
-						</span>Setting
-					</a>
-
-					<div className="navbar-dropdown is-right">
-						<a className="navbar-item">Overview</a>
-						<a className="navbar-item">Elements</a>
-						<a className="navbar-item">Components</a>
-						<hr className="navbar-divider" />
-						<div className="navbar-item">Version 0.7.1</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		aria-label="dropdown navigation">
+		<div className="navbar-menu container" />
 	</Wrapper>
 )
 
